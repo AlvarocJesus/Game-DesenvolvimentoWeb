46592ceb2299a5ca659316901da5c4f341c230e8
AlvarocJesus <alvim.coelho.jesus@gmail.com>
2021-10-05 16:08:16 -0300

    Adicionando arquivos do jogo

---
+++ scripts/game.js
@@ -0,0 +1,30 @@
+let canvas = document.getElementById("canva");
+let ctx = canvas.getContext("2d");
+
+const obj1 = {
+  x: 0,
+  y: 0,
+  width: 0,
+  height: 0,
+  color: "red",
+  speed: 0
+};
+
+const obj2 = {
+  x: 0,
+  y: 0,
+  width: 0,
+  height: 0,
+  color: "red",
+  speed: 0
+};
+
+function main() {}
+function atualiza() {}
+function desenha() {}
+function movimenta() {}
+
+main();
+atualiza();
+desenha();
+movimenta();
